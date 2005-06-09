68e01983ffc5c19948303a98fb362ca8cedb9989
jeffbeck <jeffbeck>
2005-06-09 11:18:46 +0000

    Erste Version, die meisten funktionen sind leer.

---
+++ sophomorix-sys-pgldap/modules/SophomorixSYSPgLdap.pm
@@ -0,0 +1,151 @@
+#!/usr/bin/perl -w
+# Dieses Modul (SophomorixSYSPgLdap) wurde von Rüdiger Beck erstellt
+# Es ist freie Software
+# Bei Fehlern wenden Sie sich bitte an mich.
+# jeffbeck@web.de  oder  jeffbeck@gmx.de
+
+
+package Sophomorix::SophomorixSYSPgLdap;
+require Exporter;
+
+@ISA =qw(Exporter);
+@EXPORT = qw(show_sys_modulename
+             update_gecos
+             delete_user_from_sys
+             add_class_to_sys
+             add_user_to_sys
+             get_user_auth_data
+);
+
+use Sophomorix::SophomorixBase qw ( titel
+                                    do_falls_nicht_testen
+                                  );
+
+
+
+=head1 Documentation of SophomorixSYSPgLdap.pm
+
+The module is used to write/modify sophomorix database information to
+the system wich uses postgresql as ldap backend
+
+=head2 Functions
+
+=over 4
+
+=item  I<show_sys_modulename()>
+
+Shows the name of the actually loaded module
+
+=cut
+
+
+sub show_sys_modulename {
+#    if($Conf::log_level>=2){
+       &Sophomorix::SophomorixBase::titel("SYS-DB-Module:       SophomorixSYSPgLdap.pm");
+
+#   }
+}
+
+
+
+
+=pod
+
+=item  I<update_gecos(login, first name, last name)>
+
+Updates the gecos field of login in /etc/passwd
+
+=cut
+
+sub update_gecos {
+   # nothing to do
+}
+
+=pod
+
+=item  I<delete_user_from_sys(login)>
+
+Deletes the user I<login> in F<passwd>, F<samba> and removes her home
+directory for good.
+
+=cut
+
+sub delete_user_from_sys {
+    ($login)=@_;
+    &Sophomorix::SophomorixBase::do_falls_nicht_testen(
+       # aus smbpasswd entfernen
+       "/usr/bin/smbpasswd  -x $login",
+       # Aus Benutzerdatenbank entfernen (-r: Home löschen)
+       #"userdel  -r $login",
+       # remove the users homedirectory 
+       # ???? Todo ???
+    );
+}
+
+=pod
+
+=item  I<add_class_to_sys(class, gid)>
+
+Adds the group I<class> to the system database.
+
+
+=cut
+
+sub add_class_to_sys {
+    # nothing to do
+}
+
+
+=pod
+
+=item  I<add_user_to_sys(class)>
+
+Adds the user I<user> to the system database.
+
+
+=cut
+
+sub add_user_to_sys {
+    # nothing to do
+}
+
+
+
+=pod
+
+=item  I<get_user_auth_data(login)>
+
+Retrieves data as in getpwnam Does getpwnam also work with ldap, sql? 
+
+
+=cut
+sub get_user_auth_data {
+    # should be OK so ???
+    my ($login) = @_;
+    # Abfragen der /etc/passwd
+    my @data = getpwnam("$login");
+    return @data;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+# OK
+
+
+
+
+
+
+
+# ENDE DER DATEI
+# Wert wahr=1 zurückgeben
+1;
