26b0b2fb4d38a4abca35028f86df1f284c9c35ec
jeffbeck <jeffbeck>
2005-07-09 22:59:24 +0000

    erste Version

---
+++ sophomorix-base/modules/SophomorixConfig.pm
@@ -0,0 +1,73 @@
+#!/usr/bin/perl -w
+# Dieses Modul (SophomorixBase.pm) wurde von Rüdiger Beck erstellt
+# Es ist freie Software
+# Bei Fehlern wenden Sie sich bitte an mich.
+# jeffbeck@web.de  oder  jeffbeck@gmx.de
+
+package Sophomorix::SophomorixConfig;
+require Exporter;
+use Time::Local;
+use Time::localtime;
+
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw( );
+@EXPORT = qw( 
+              );
+
+
+my $conf="/etc/sophomorix/user/sophomorix.conf";
+if (not -e $conf){
+    print "ERROR: $conf not found!\n";
+    exit;
+}
+
+# Einlesen der Konfigurationsdatei
+{ package Conf ; do "$conf"}
+# Die in sophomorix.conf als global (ohne my) deklarierten Variablen
+# können nun mit $Conf::Variablenname angesprochen werden
+
+
+=head1 Documentation of SophomorixConfig.pm
+
+For documentation see module SophomorixAPI (perldoc Sophomorix:.SophomorixAPI).
+
+=cut
+
+
+my $develconf="/usr/share/sophomorix/devel/sophomorix-devel.conf";
+if (not -e $develconf){
+    print "ERROR: $develconf not found!\n";
+    exit;
+}
+
+# Einlesen der Konfigurationsdatei für Entwickler
+#{ package DevelConf ; do "/etc/sophomorix/devel/user/sophomorix-devel.conf"}
+{ package DevelConf ; do "$develconf"}
+
+
+
+
+
+
+
+
+
+sub show_language {
+#    if($Conf::log_level>=2){
+       &titel("Language:   $Conf::lang");
+#   }
+}
+
+
+
+
+
+
+
+
+
+# ENDE DER DATEI
+# Wert wahr=1 zurückgeben
+1;
