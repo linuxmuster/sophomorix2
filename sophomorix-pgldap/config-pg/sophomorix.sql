e1dcc7f740355bd7b09b8fa629e7d7f39a3c490b
jeffbeck <jeffbeck>
2005-11-06 18:29:46 +0000

    adjusted entries for classdata

--- sophomorix-pgldap/config-pg/sophomorix.sql
+++ sophomorix-pgldap/config-pg/sophomorix.sql
@@ -1305,19 +1305,16 @@ CREATE VIEW userdata AS
 
 CREATE TABLE class_details (
 id integer NOT NULL,
-quota integer,
+quota character varying(255),
 schooltype character varying(255),
 department character varying(255),
-status boolean,
-enddate date,
-longname character varying(255),
-maxmebers integer,
-"type" integer
+mailalias boolean,
+"type" character varying(255)
 );
 
 CREATE TABLE project_details (
 id integer NOT NULL,
-addquota integer,
+addquota character varying(255),
 schooltype character varying(255),
 department character varying(255),
 status boolean,
@@ -1732,4 +1729,4 @@ INSERT INTO posix_account VALUES (1, 1000, 'NextFreeUnixId', 1000, '', 'NextFree
 
 CREATE VIEW projectdata AS SELECT project_details.id, project_details.addquota, project_details.schooltype, project_details.department, project_details.status, project_details.enddate, project_details.longname, project_details.maxmebers, project_details.type, groups.gid, groups.gidnumber, samba_group_mapping.sambasid, samba_group_mapping.sambagrouptype, samba_group_mapping.displayname, samba_group_mapping.description, samba_group_mapping.sambasidlist FROM project_details FULL JOIN groups ON project_details.id = groups.id FULL JOIN samba_group_mapping ON samba_group_mapping.id=groups.id;
 
-CREATE VIEW classdata AS SELECT class_details.id, class_details.quota, class_details.schooltype, class_details.department, class_details.status, class_details.enddate, class_details.longname, class_details.maxmebers, class_details.type, groups.gid, groups.gidnumber, samba_group_mapping.sambasid, samba_group_mapping.sambagrouptype, samba_group_mapping.displayname, samba_group_mapping.description, samba_group_mapping.sambasidlist FROM class_details FULL JOIN groups ON class_details.id = groups.id FULL JOIN samba_group_mapping ON samba_group_mapping.id=groups.id;
+CREATE VIEW classdata AS SELECT class_details.id, class_details.quota, class_details.schooltype, class_details.department, class_details.type, groups.gid, groups.gidnumber, samba_group_mapping.sambasid, samba_group_mapping.sambagrouptype, samba_group_mapping.displayname, samba_group_mapping.description, samba_group_mapping.sambasidlist FROM class_details FULL JOIN groups ON class_details.id = groups.id FULL JOIN samba_group_mapping ON samba_group_mapping.id=groups.id;
