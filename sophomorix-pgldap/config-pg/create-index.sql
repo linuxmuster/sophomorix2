29238315f9d59c8ecf3f3402815edff5b270d719
jeffbeck <jeffbeck>
2006-11-13 20:56:45 +0000

    update to Meilick indices

--- sophomorix-pgldap/config-pg/create-index.sql
+++ sophomorix-pgldap/config-pg/create-index.sql
@@ -2,7 +2,16 @@
 \set AUTOCOMMIT off
 BEGIN WORK;
 
-CREATE INDEX idx_leo_oc_name ON ldap_entry_objclasses (oc_name);
+CREATE INDEX leo_oc_name_idx ON ldap_entry_objclasses (oc_name);
+--CREATE INDEX leo_id_idx ON ldap_entry_objclasses (id);
+CREATE INDEX leo_entry_id_idx ON ldap_entry_objclasses (entry_id);
+CREATE INDEX le_id_idx ON ldap_entries (id);
+CREATE INDEX le_keyval_idx ON ldap_entries (keyval);
+CREATE INDEX le_oc_map_id_idx ON ldap_entries (oc_map_id);
+CREATE INDEX g_id_idx ON groups (id);
+CREATE INDEX pa_id_idx on posix_account(id);
+
+ANALYZE;
 
 COMMIT WORK;
 \set AUTOCOMMIT on
--- sophomorix-pgldap/config-pg/drop-index.sql
+++ sophomorix-pgldap/config-pg/drop-index.sql
@@ -1,4 +1,10 @@
 # $Id$
 # The following indices will be dropped (if they exist)
-#idx_leo_oc_name
-idx_nonexisting
+leo_oc_name_idx
+#leo_id_idx
+leo_entry_id_idx
+le_id_idx
+le_keyval_idx
+le_oc_map_id_idx
+g_id_idx
+pa_id_idx
