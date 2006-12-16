76e16b3381b13ac3b6a2472eaf50a5b5794272f4
jeffbeck <jeffbeck>
2006-12-16 15:03:02 +0000

    make /etc/slapd.conf owned by root again for slapd 2.3.29

--- sophomorix-base/modules/SophomorixBase.pm
+++ sophomorix-base/modules/SophomorixBase.pm
@@ -503,6 +503,7 @@ sub make_some_files_root_only {
     } else {
        # add more files here
        @filelist=("/etc/ldap.secret",
+                  "/etc/ldap/slapd.conf",
                   "/etc/smbldap-tools/smbldap.conf",
                   "/etc/smbldap-tools/smbldap_bind.conf",
                   "/etc/imap.secret"
