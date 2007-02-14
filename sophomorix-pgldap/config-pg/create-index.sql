57d9860548115e617f980fefc3508e428a82de1c
jeffbeck <jeffbeck>
2007-02-14 22:07:46 +0000

    update

--- sophomorix-pgldap/config-pg/create-index.sql
+++ sophomorix-pgldap/config-pg/create-index.sql
@@ -5,12 +5,14 @@ BEGIN WORK;
 --CREATE INDEX leo_oc_name_idx ON UPPER(ldap_entry_objclasses) (oc_name);
 --CREATE INDEX leo_id_idx ON ldap_entry_objclasses (id);
 CREATE INDEX leo_entry_id_idx ON ldap_entry_objclasses (entry_id);
+CREATE INDEX leo_oc_name_idx ON ldap_entry_objclasses (upper(oc_name));
+
 CREATE INDEX le_id_idx ON ldap_entries (id);
 CREATE INDEX le_keyval_idx ON ldap_entries (keyval);
 CREATE INDEX le_oc_map_id_idx ON ldap_entries (oc_map_id);
+
 CREATE INDEX g_id_idx ON groups (id);
 CREATE INDEX pa_id_idx on posix_account(id);
-CREATE INDEX oc_name_idx ON ldap_entry_objclasses (upper(oc_name));
 ANALYZE;
 
 COMMIT WORK;
