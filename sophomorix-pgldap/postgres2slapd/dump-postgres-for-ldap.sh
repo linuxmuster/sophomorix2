7223fba7fc66c582b7af5fab58fd029d4444e518
jeffbeck <jeffbeck>
2007-06-11 13:54:39 +0000

    update with options

--- sophomorix-pgldap/postgres2slapd/dump-postgres-for-ldap.sh
+++ sophomorix-pgldap/postgres2slapd/dump-postgres-for-ldap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo 'select groups.gidnumber,gid,sambasid,sambagrouptype,displayname,description,sambasidlist from groups, samba_group_mapping where groups.id=samba_group_mapping.id;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$ > /tmp/groups.sql
+dump_dir=$1
+mkdir -p $dump_dir
 
-echo 'SELECT * from samba_sam_account,posix_account where posix_account.id=samba_sam_account.id;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$  > /tmp/accounts.sql
+echo Dumping groups.sql to $dump_dir
+echo 'select groups.gidnumber,gid,sambasid,sambagrouptype,displayname,description,sambasidlist from groups, samba_group_mapping where groups.id=samba_group_mapping.id;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$ > $dump_dir/groups.sql
+
+echo Dumping accounts.sql to $dump_dir
+echo 'SELECT * from samba_sam_account,posix_account where posix_account.id=samba_sam_account.id;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$  > $dump_dir/accounts.sql
 
 #echo 'select groups.gid,memberuid from groups, groups_users WHERE groups_users.gidnumber=groups.gidnumber;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)"| grep -v ^$ > /tmp/groups_users.sql
 
-echo 'select groups.gid,posix_account.uid,memberuidnumber from groups, groups_users, posix_account WHERE groups_users.gidnumber=groups.gidnumber AND posix_account.uidnumber=groups_users.memberuidnumber;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "Zeilen)" | grep -v "rows)" | grep -v ^$ > /tmp/groups_users.sql
+echo Dumping groups_users.sql to $dump_dir
+echo 'select groups.gid,posix_account.uid,memberuidnumber from groups, groups_users, posix_account WHERE groups_users.gidnumber=groups.gidnumber AND posix_account.uidnumber=groups_users.memberuidnumber;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "Zeilen)" | grep -v "rows)" | grep -v ^$ > $dump_dir/groups_users.sql
