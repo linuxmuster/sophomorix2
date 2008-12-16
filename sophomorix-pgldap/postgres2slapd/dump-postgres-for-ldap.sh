b80bf0a39379e75c93a6be5c140fbaa323681b2d
jeffbeck <jeffbeck>
2008-12-16 14:01:16 +0000

    test only: this should also dump primary memberships

--- sophomorix-pgldap/postgres2slapd/dump-postgres-for-ldap.sh
+++ sophomorix-pgldap/postgres2slapd/dump-postgres-for-ldap.sh
@@ -16,3 +16,7 @@ echo 'SELECT * from samba_sam_account,posix_account where posix_account.id=samba
 
 echo "   * Dumping groups_users.sql to $dump_dir"
 echo 'select groups.gid,posix_account.uid,memberuidnumber from groups, groups_users, posix_account WHERE groups_users.gidnumber=groups.gidnumber AND posix_account.uidnumber=groups_users.memberuidnumber;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "Zeilen)" | grep -v "rows)" | grep -v ^$ > $dump_dir/groups_users.sql
+
+echo "   * Appending dump of primary groups to $dump_dir"
+echo 'select groups.gid,uid,uidnumber from groups,posix_account WHERE groups.gidnumber=posix_account.gidnumber;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "Zeilen)" | grep -v "rows)" | grep -v ^$ >> $dump_dir/groups_users.sql
+
