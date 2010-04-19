72fe1298bcc451f9d7e39a2ec16030b50e9bc876
jeffbeck <jeffbeck>
2010-04-19 07:29:08 +0000

    update

---
+++ sophomorix-pgldap/presentation/LDAP in paedML.mm
@@ -0,0 +1,23 @@
+<map version="0.7.1">
+<node TEXT="LDAP in paedML">
+<node TEXT="Vortrag" POSITION="right">
+<node TEXT="Zusammenhang posgresql und openldap">
+<node TEXT="Vorteile"/>
+<node TEXT="Nachteile"/>
+</node>
+<node TEXT="Abfrage des ldap">
+<node TEXT="am server">
+<node TEXT="ldapsearch"/>
+</node>
+<node TEXT="vom Schulnetz">
+<node TEXT="ldapsearch"/>
+<node TEXT="GUI: luma"/>
+</node>
+<node TEXT="Extern">
+<node TEXT="Zukunft: auf Liste von IP&apos;s zugelassen"/>
+</node>
+</node>
+<node TEXT="Einbindung des moodle"/>
+</node>
+</node>
+</map>
