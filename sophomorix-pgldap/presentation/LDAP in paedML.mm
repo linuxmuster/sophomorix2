08ddbeb61faf0be9392a58e22e1e9508f1dc3623
jeffbeck <jeffbeck>
2010-04-19 12:45:52 +0000

    update

--- sophomorix-pgldap/presentation/LDAP in paedML.mm
+++ sophomorix-pgldap/presentation/LDAP in paedML.mm
@@ -1,23 +1,50 @@
-<map version="0.7.1">
-<node TEXT="LDAP in paedML">
-<node TEXT="Vortrag" POSITION="right">
-<node TEXT="Zusammenhang posgresql und openldap">
-<node TEXT="Vorteile"/>
-<node TEXT="Nachteile"/>
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1271680268562" MODIFIED="1271680268562" TEXT="LDAP in paedML">
+<node CREATED="1271680268563" MODIFIED="1271680268563" POSITION="right" TEXT="Vortrag">
+<node CREATED="1271680268563" MODIFIED="1271680268563" TEXT="Zusammenhang posgresql und openldap">
+<node CREATED="1271680268563" MODIFIED="1271680268563" TEXT="Vorteile"/>
+<node CREATED="1271680268563" MODIFIED="1271680268563" TEXT="Nachteile"/>
 </node>
-<node TEXT="Abfrage des ldap">
-<node TEXT="am server">
-<node TEXT="ldapsearch"/>
-</node>
-<node TEXT="vom Schulnetz">
-<node TEXT="ldapsearch"/>
-<node TEXT="GUI: luma"/>
-</node>
-<node TEXT="Extern">
-<node TEXT="Zukunft: auf Liste von IP&apos;s zugelassen"/>
+<node CREATED="1271680268563" MODIFIED="1271680268563" TEXT="Abfrage des ldap">
+<node CREATED="1271680268563" ID="Freemind_Link_140303233" MODIFIED="1271680268563" TEXT="am server">
+<node CREATED="1271680418878" ID="_" MODIFIED="1271680421695" TEXT="Alles">
+<node CREATED="1271680268563" ID="Freemind_Link_905276579" MODIFIED="1271680411422" TEXT="ldapsearch -x"/>
+<node CREATED="1271680702798" ID="Freemind_Link_1133147374" MODIFIED="1271680998356" TEXT="ldapsearch -x -D cn=admin,dc=bszleo,dc=de -y /etc/ldap.secret">
+<node CREATED="1271681461510" ID="Freemind_Link_678694689" MODIFIED="1271681475106" TEXT="-D binddn"/>
+<node CREATED="1271681476278" ID="Freemind_Link_132791215" MODIFIED="1271681493729" TEXT="-y passwort-Datei"/>
 </node>
 </node>
-<node TEXT="Einbindung des moodle"/>
+<node CREATED="1271680531679" ID="Freemind_Link_572059927" MODIFIED="1271680537750" TEXT="Einzelne User">
+<node CREATED="1271680543295" ID="Freemind_Link_1609343426" MODIFIED="1271680550816" TEXT="smbldap-usershow user"/>
+<node CREATED="1271681709521" ID="Freemind_Link_333037376" MODIFIED="1271681726809" TEXT="zu obigem ldapsearch hinzu: uid=xyz"/>
+</node>
+<node CREATED="1271682975831" ID="Freemind_Link_1670828970" MODIFIED="1271682983224" TEXT=".ldaprc anlegen"/>
+</node>
+<node CREATED="1271680268563" ID="Freemind_Link_616105982" MODIFIED="1271680268563" TEXT="vom Schulnetz">
+<node CREATED="1271680268563" MODIFIED="1271680268563" TEXT="ldapsearch"/>
+<node CREATED="1271680268563" ID="Freemind_Link_1459920899" MODIFIED="1271680268563" TEXT="GUI: luma">
+<node CREATED="1271681797757" ID="Freemind_Link_206278650" MODIFIED="1271681804957" TEXT="aptitude install luma"/>
+<node CREATED="1271682199359" FOLDED="true" ID="Freemind_Link_804559674" MODIFIED="1271682208260" TEXT="Verbindungen einrichten">
+<node CREATED="1271682212500" ID="Freemind_Link_1459550240" MODIFIED="1271682217508" TEXT="Server-Admin"/>
+<node CREATED="1271682217767" ID="Freemind_Link_999841654" MODIFIED="1271682225924" TEXT="Server-User"/>
+</node>
+<node CREATED="1271682231110" ID="Freemind_Link_1210346351" MODIFIED="1271682235768" TEXT="Als Admin">
+<node CREATED="1271682249054" ID="Freemind_Link_1579802397" MODIFIED="1271682355339" TEXT="Browser"/>
+<node CREATED="1271682239641" ID="Freemind_Link_1944942699" MODIFIED="1271682248797" TEXT="Schema-Browser"/>
+<node CREATED="1271682355879" ID="Freemind_Link_190106384" MODIFIED="1271682360796" TEXT="Usermanagement"/>
+</node>
+</node>
+<node CREATED="1271682820130" ID="Freemind_Link_1677222490" MODIFIED="1271682824707" TEXT="Alternativen">
+<node CREATED="1271682828095" ID="Freemind_Link_404249002" MODIFIED="1271682831966" TEXT="id Name"/>
+<node CREATED="1271682843878" ID="Freemind_Link_183848311" MODIFIED="1271682848991" TEXT="getent passwd"/>
+</node>
+</node>
+<node CREATED="1271680268563" MODIFIED="1271680268563" TEXT="Extern">
+<node CREATED="1271680268563" MODIFIED="1271680268563" TEXT="Zukunft: auf Liste von IP&apos;s zugelassen"/>
+</node>
+</node>
+<node CREATED="1271680268563" ID="Freemind_Link_1400011421" MODIFIED="1271683006722" TEXT="Einbindung des moodle"/>
 </node>
 </node>
 </map>
