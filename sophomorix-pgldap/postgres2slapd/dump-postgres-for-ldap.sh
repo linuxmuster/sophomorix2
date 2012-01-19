2ca0a1839767e58e1ed0393a9792fb54e475259d
Rüdiger Beck <jeffbeck@web.de>
2012-01-19 19:31:31 +0100

    dump to accounts.sql: changed select to use table userdata which contains all in future needed values

--- sophomorix-pgldap/postgres2slapd/dump-postgres-for-ldap.sh
+++ sophomorix-pgldap/postgres2slapd/dump-postgres-for-ldap.sh
@@ -10,7 +10,8 @@ echo "   * Dumping groups.sql to $dump_dir"
 echo 'select groups.gidnumber,gid,sambasid,sambagrouptype,displayname,description,sambasidlist from groups, samba_group_mapping where groups.id=samba_group_mapping.id;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$ > $dump_dir/groups.sql
 
 echo "   * Dumping accounts.sql to $dump_dir"
-echo 'SELECT * from samba_sam_account,posix_account where posix_account.id=samba_sam_account.id;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$  > $dump_dir/accounts.sql
+#echo 'SELECT * from samba_sam_account,posix_account where posix_account.id=samba_sam_account.id;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$  > $dump_dir/accounts.sql
+echo 'SELECT id,sambasid,cn,sambalmpassword,sambantpassword,sambapwdlastset,sambalogontime,sambalogofftime,sambakickofftime,sambapwdcanchange,sambapwdmustchange,sambaacctflags,displayname,sambahomepath,sambahomedrive,sambalogonscript,sambaprofilepath,description,sambauserworkstations,sambaprimarygroupsid,sambadomainname,sambamungeddial,sambabadpasswordcount,sambabadpasswordtime,sambapasswordhistory,sambalogonhours,id,uidnumber,uid,gidnumber,firstname,surname,homedirectory,gecos,loginshell,userpassword,description from userdata;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)" | grep -v ^$  > $dump_dir/accounts.sql
 
 #echo 'select groups.gid,memberuid from groups, groups_users WHERE groups_users.gidnumber=groups.gidnumber;' | psql -U postgres -d ldap | grep -v "\-\-+\-\-" | grep -v "rows)" | grep -v "Zeilen)"| grep -v ^$ > /tmp/groups_users.sql
 
